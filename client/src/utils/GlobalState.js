3888e8cec29595c2d57b918dec1890caee03bf4f
psurer <surereker@icloud.com>
2021-08-22 21:46:04 -0400

    service worker and utils done

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Provider } from 'react-redux';
+import store from './store';
+
+const StoreProvider = ({...props }) => {
+  return <Provider store={store} {...props} />;
+};
+
+export { StoreProvider };
+
